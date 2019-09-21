e51ed0815996b90c9797b12be9374d2ddb9e994b
Doctorbingbing <cparizea@uci.edu>
2019-09-21 15:39:56 -0700

    JS | Final Project - Christian Parizeau

--- includes/main.js
+++ includes/main.js
@@ -25,10 +25,10 @@ function initiateApp(){
 	*/
 	makeGallery(pictures);
 	addModalCloseHandler();
+	$('#gallery').sortable();
 }
 function makeGallery(imageArray){
 	//use loops and jquery dom creation to make the html structure inside the #gallery section
-
 	//create a loop to go through the images in the imageArray
 		//create the elements needed for each picture, store the elements in variable
 
@@ -37,14 +37,30 @@ function makeGallery(imageArray){
 		//append the element to the #gallery section
 	
 	// side note: make sure to remove the hard coded html in the index.html when you are done!
-
+	for (index = 0; index < imageArray.length; index++){
+		var currentPhoto = imageArray[index];
+		var imgDiv= $('<figure>',{
+			'class' : 'imageGallery col-xs-12 col-sm-6 col-md-4 ui-state-default ui-sortable-handle',
+			'style': 'background-image:URL('+currentPhoto+')',
+			'data-image-url': currentPhoto //Work around for sortable changing style property
+		});
+		var caption = $('<figcaption>',{
+			text: currentPhoto.slice( currentPhoto.indexOf('/')+1)
+		});
+		$(imgDiv).append(caption);
+		imgDiv.click( displayImage )
+		$('#gallery').append(imgDiv);
+		
+	}
 }
 
 function addModalCloseHandler(){
 	//add a click handler to the img element in the image modal.  When the element is clicked, close the modal
+	$('.modal-body > img').click( function(){
+		$('#galleryModal').modal('hide');
+	});
 	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp	
 }
-
 function displayImage(){
 	//find the url of the image by grabbing the background-image source, store it in a variable
 	//grab the direct url of the image by getting rid of the other pieces you don't need
@@ -58,6 +74,16 @@ function displayImage(){
 
 	//show the modal with JS.  Check for more info here: 
 	//https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
+	var completeURL = $(this).attr('data-image-url');
+    console.log("TCL: displayImage -> completeURL", completeURL);
+	var shortenedURL = completeURL.slice(completeURL.indexOf('/')+1);
+	console.log("TCL: displayImage -> shortenedURL", shortenedURL);
+	var imageName = shortenedURL.slice(0,shortenedURL.indexOf('.'));
+    console.log("TCL: displayImage -> imageName", imageName)
+	
+	$('.modal-title').text(imageName);
+	$('.modal-body > img').attr('src',completeURL);
+	$('#galleryModal').modal();
 }
 
 
